56b586808630d0c2fcff14c477abbe660c5d54ca
Fabio Souza <fabmacsouza@gmail.com>
2020-02-11 17:42:11 -0300

    correcoes: update em categoriasservices e contrutor de pagamento

--- src/main/java/com/fabiosouza/cursomc/services/ClienteService.java
+++ src/main/java/com/fabiosouza/cursomc/services/ClienteService.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fabiosouza.cursomc.domain.Cliente;
-import com.fabiosouza.cursomc.domain.Cliente;
 import com.fabiosouza.cursomc.dto.ClienteDTO;
 import com.fabiosouza.cursomc.repositories.ClienteRepository;
 import com.fabiosouza.cursomc.services.exceptions.DataIntegrityException;
