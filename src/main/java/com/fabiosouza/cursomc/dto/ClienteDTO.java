6c086d1b4680a4310d24f508e0bdf0b01b2d9005
Fabio Souza <fabmacsouza@gmail.com>
2020-02-12 21:09:01 -0300

    Validacao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/fabiosouza/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/fabiosouza/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.fabiosouza.cursomc.domain.Cliente;
+import com.fabiosouza.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
