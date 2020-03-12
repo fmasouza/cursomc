93e7cd17421073a562f62fbef5b89746817a5457
Fabio Souza <fabmacsouza@gmail.com>
2020-03-12 20:57:05 -0300

    Refresh Token

--- src/main/java/com/fabiosouza/cursomc/resources/PedidoResource.java
+++ src/main/java/com/fabiosouza/cursomc/resources/PedidoResource.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.fabiosouza.cursomc.domain.Categoria;
 import com.fabiosouza.cursomc.domain.Pedido;
-import com.fabiosouza.cursomc.dto.CategoriaDTO;
 import com.fabiosouza.cursomc.services.PedidoService;
 
 @RestController
--- src/main/java/com/fabiosouza/cursomc/services/PedidoService.java
+++ src/main/java/com/fabiosouza/cursomc/services/PedidoService.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.fabiosouza.cursomc.domain.Categoria;
 import com.fabiosouza.cursomc.domain.Cliente;
 import com.fabiosouza.cursomc.domain.ItemPedido;
 import com.fabiosouza.cursomc.domain.PagamentoComBoleto;
